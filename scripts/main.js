ef774dd1a7e4ca01039ad3d7d8596c68df42b599
mineralquell@github.com <findamy@DESKTOP-HT1LDJ9.localdomain>
2020-07-24 22:49:45 +0200

    page

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,3 @@
-
-
-
-
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -14,4 +10,40 @@ myImage.onclick = function() {
 }
 /*document.querySelector('img').onclick = function() {
    alert('MEOW');
+}*/
+
+let myButton = document.getElementById('catname');
+//let myHeading = document.querySelector('h1');
+
+
+myButton.onclick = function(){
+	alert("the cat's name is pocchi");
+}
+
+
+/*
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
 }*/
